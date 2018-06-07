a2140cb6c6a9c377111cb4b963c8060d3e4e6ff0
Daniel Schulz <karleule@posteo.de>
2018-06-08 00:00:25 +0200

    copy to doc folder

---
+++ docs/precache-manifest.625d15460bba3fb53f2d1739e1a3503e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa7e9014bfb6a4240d1",
+    "url": "/js/chunk-vendors.baa7e901.js"
+  },
+  {
+    "revision": "86325950ba001c308653",
+    "url": "/js/app.86325950.js"
+  },
+  {
+    "revision": "b856722a29867fc29be6bc25c646108b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa7e9014bfb6a4240d1",
+    "url": "/css/chunk-vendors.5bc2d347.css"
+  },
+  {
+    "revision": "86325950ba001c308653",
+    "url": "/css/app.adcc6b4a.css"
+  }
+];
